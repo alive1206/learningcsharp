870b912cf3803866ad8096f706676e2a0c44880f
BusySnow <thanglb.bi11-243@st.usth.edu.vn>
2024-09-04 21:13:14 +0700

    switch example

---
+++ switch/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SwitchApplication
+{
+    class Program{
+        static void Main(string[] args)
+        {
+            //Type casting
+            Console.WriteLine("Choose your lucky number from 1 to 10: ");
+            int choice = Convert.ToInt32(Console.ReadLine());    
+
+            switch (choice)
+            {
+                case 1:
+                Console.WriteLine("Congrats! You will be lucky today.");
+                break;
+                case 2:
+                Console.WriteLine("Congrats! You will be lucky today.");
+                break;
+                case 3:
+                Console.WriteLine("Congrats! You will be lucky today.");
+                break;
+                case 4:
+                Console.WriteLine("Congrats! You will be lucky today.");
+                break;
+                case 5:
+                Console.WriteLine("Congrats! You will be lucky today.");
+                break;
+                case 6:
+                Console.WriteLine("Congrats! You will be lucky today.");
+                break;
+                case 7:
+                Console.WriteLine("Congrats! You will be lucky today.");
+                break;
+                case 8:
+                Console.WriteLine("Congrats! You will be lucky today.");
+                break;
+                case 9:
+                Console.WriteLine("Congrats! You will be lucky today.");
+                break;
+                case 10:
+                Console.WriteLine("Congrats! You will be lucky today.");
+                break;
+            }
+
+
+        }
+    }
+}
