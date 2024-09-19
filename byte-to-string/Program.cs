4cf7bc5efe81d7ce3e1d7e80b15e09ef6d363cc1
BusySnow <thanglb.bi11-243@st.usth.edu.vn>
2024-09-19 14:56:12 +0700

    add: encode

---
+++ byte-to-string/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhập một chuỗi:");
+        string input= Console.ReadLine();
+        var asciiInputByte = Encoding.ASCII.GetBytes(input);        
+        Console.WriteLine($"Mã hóa ASCII:\n{string.Join(" ",asciiInputByte)}");
+        var asciiInputString =  Encoding.ASCII.GetString(asciiInputByte);
+        Console.WriteLine($"ASCII String:\n{asciiInputString}\n");
+
+
+        var utf8InputByte = Encoding.UTF8.GetBytes(input);
+        Console.WriteLine($"Mã hóa UTF8:\n{string.Join(" ",utf8InputByte)}");
+        var utf8InputString =  Encoding.ASCII.GetString(utf8InputByte);
+        Console.WriteLine($"UTF8 String:\n{utf8InputString}");
+        
+    }
+}
---
+++ encode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhập một chuỗi:");
+        string input= Console.ReadLine();
+        var asciiInput = Encoding.ASCII.GetBytes(input);
+        Console.WriteLine($"Mã hóa ASCII:\n{string.Join(" ",asciiInput)}");
+        var utf8Input = Encoding.UTF8.GetBytes(input);
+        Console.WriteLine($"Mã hóa UTF8:\n{string.Join(" ",utf8Input)}");
+        
+    }
+}
