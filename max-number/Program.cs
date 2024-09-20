1a86d353c05d7a8b5bcb4ab6e87421b8fb0d6b34
Thang Le <thangle12062002@gmail.com>
2024-09-20 11:18:46 +0700

    add:max -number

---
+++ max-number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhập số bit n:");
+        int n = int.Parse(Console.ReadLine());
+        int maxNumber = GetMaxNumber(n);
+        Console.WriteLine($"Số lớn nhất có thể biểu diễn bằng {n} bit là: {maxNumber}");
+    }
+
+    public static int GetMaxNumber(int n)
+    {
+        if (n < 1)
+        {
+            throw new ArgumentException("n phải lớn hơn 0.");
+        }
+        return (1 << n) - 1; //2^n -1
+    }
+}
